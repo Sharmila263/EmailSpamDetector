0b0650b7c31036017cbc8c768646bbe5e6d1d887
BEERAKA SHARMILA <139761386+Sharmila263@users.noreply.github.com>
2024-08-05 19:57:20 +0530

    email_spam_detection.py

--- email_spam_detection.py
+++ email_spam_detection.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import sklearn 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
