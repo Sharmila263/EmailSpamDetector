2a5ea281bef2bf6996cb46ae7311024ee4cdb6db
BEERAKA SHARMILA <139761386+Sharmila263@users.noreply.github.com>
2024-08-09 18:43:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 import pandas as pd
 import numpy as np
 import sklearn 
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
+from sklearn.naive_bayes import MultinomialNB
 import streamlit as st
 
 # Load and preprocess data
 csv_url= "https://raw.githubusercontent.com/Sharmila263/EmailSpamDetector/main/spam%20mail.csv"
 data = pd.read_csv(csv_url)
 data['category'] = data['Category'].map({'spam': 1, 'ham': 0})
-vectorizer = CountVectorizer()
+
+# Use TfidfVectorizer 
+vectorizer = TfidfVectorizer()
 x = vectorizer.fit_transform(data['Masseges'])
 y = data['category']
 
 # Split data
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
-# Train model
-model = SVC()
+# Train a Naive Bayes model
+model = MultinomialNB()
 model.fit(x_train, y_train)
 
 # Evaluate model
